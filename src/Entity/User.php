749f770ecbef6e86878fb0d7a6782d0f84972f5a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-10 19:28:46 +0200

    Add __toString method to User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,6 +36,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $password;
 
+    public function __toString()
+    {
+        return $this->username;
+    }
+
     public function getId(): ?int
     {
         return $this->id;

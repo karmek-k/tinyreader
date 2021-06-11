1cabb0db32a596e6bb3f0180714b9750e6b37975
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-11 15:53:10 +0200

    Add custom labels to FeedSource form

--- src/Form/FeedSourceType.php
+++ src/Form/FeedSourceType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\FeedSource;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,12 @@ class FeedSourceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('url');
+            ->add('name', TextType::class, [
+                'label' => 'Feed source name',
+            ])
+            ->add('url', UrlType::class, [
+                'label' => 'RSS feed URL',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

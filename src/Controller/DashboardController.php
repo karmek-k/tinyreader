58c504e97df59c41798cb802deeadf6ca7650c35
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-11 15:38:18 +0200

    Change Route attribute of DashboardController

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/dashboard')]
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'dashboard')]
+    #[Route('/', name: 'dashboard_index')]
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [

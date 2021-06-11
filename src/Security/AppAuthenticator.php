021b3a149b6fff24493239f67d5178bdc756db74
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-11 15:19:45 +0200

    Create DashboardController

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,10 +49,8 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-        
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+
+        return new RedirectResponse($this->urlGenerator->generate('dashboard'));
     }
 
     protected function getLoginUrl(Request $request): string

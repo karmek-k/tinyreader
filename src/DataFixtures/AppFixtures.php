3f3c5f0d9e69cd67158f317b27c2dd5c9c75e620
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-12 12:12:28 +0200

    Add the sample source to user in data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,17 +17,18 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user
-            ->setUsername('user')
-            ->setPassword($this->hasher->hashPassword($user, '12345'));
-        $manager->persist($user);
-
         $feedSource = new FeedSource();
         $feedSource
             ->setName('Infor Kadry')
             ->setUrl('https://rss.infor.pl/rss/kadry_artykuly.xml');
         $manager->persist($feedSource);
+
+        $user = new User();
+        $user
+            ->setUsername('user')
+            ->setPassword($this->hasher->hashPassword($user, '12345'))
+            ->addSource($feedSource);
+        $manager->persist($user);
         
         $article = new Article();
         $article

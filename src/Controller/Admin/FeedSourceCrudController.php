19cffed402960c5fbb26f51495393c265af2dd28
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-12 15:19:46 +0200

    Customice CRUD controllers

--- src/Controller/Admin/FeedSourceCrudController.php
+++ src/Controller/Admin/FeedSourceCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\FeedSource;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
 class FeedSourceCrudController extends AbstractCrudController
 {
@@ -12,14 +15,12 @@ class FeedSourceCrudController extends AbstractCrudController
         return FeedSource::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            UrlField::new('url'),
         ];
     }
-    */
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +14,11 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('username'),
+            AssociationField::new('sources'),
         ];
     }
-    */
 }

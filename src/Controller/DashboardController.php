1d7b26f2c718bc0e9b399be243b4a8a5dce433e4
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-13 19:00:38 +0200

    Create dashboard_reload route

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\FeedSource;
+use App\Service\ArticleFactory;
+use App\Service\RssReader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +21,35 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'sources' => $sources,
         ]);
+    }
+
+    #[Route('/reload', name: 'dashboard_reload')]
+    public function reload(ArticleFactory $articleFactory, RssReader $rss): Response
+    {
+        /** @var FeedSource[] */
+        $sources = $this->getUser()->getSources();
+
+        $em = $this->getDoctrine()->getManager();
+
+        // TODO: this is horribly bad, improve this & move to an external service
+        foreach ($sources as $source) {
+            $feed = $rss->read($source->getUrl());
+            $rssArticles = $articleFactory->fromFeedAll($feed);
+
+            $storedArticles = $source->getArticles()->toArray();
+
+            foreach ($rssArticles as $rssArticle) {
+                if (!in_array($rssArticle, $storedArticles)) {
+                    $em->persist($rssArticle);
+                    $source->addArticle($rssArticle);
+                }   
+            }
+        }
+
+        $em->flush();
+
+        $this->addFlash('success', 'Feed has been reloaded');
+
+        return $this->redirectToRoute('dashboard_index');
     }
 }

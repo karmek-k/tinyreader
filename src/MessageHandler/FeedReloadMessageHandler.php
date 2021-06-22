c34a5301fac40f33aa425dc256b91bef9ea810f5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-22 16:59:06 +0200

    Create FeedReloadMessageHandler

--- src/MessageHandler/FeedReloadMessageHandler.php
+++ src/MessageHandler/FeedReloadMessageHandler.php
@@ -3,12 +3,24 @@
 namespace App\MessageHandler;
 
 use App\Message\FeedReloadMessage;
+use App\Repository\UserRepository;
+use App\Service\ArticleLoader;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class FeedReloadMessageHandler implements MessageHandlerInterface
 {
-    public function __invoke(FeedReloadMessage $message)
+    public function __construct(
+        private ArticleLoader $loader,
+        private UserRepository $userRepo,
+    ) {}
+
+    public function __invoke(FeedReloadMessage $message): void
     {
-        // do something with your message
+        $user = $this->userRepo->find($message->getUserId());
+        if ($user === null) {
+            return;
+        }
+
+        $this->loader->loadNew($user->getSources());
     }
 }
